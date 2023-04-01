01767e57c211b498d9173baa72ce6ca6684350a8
jms1192 <62626527+jms1192@users.noreply.github.com>
2023-03-31 23:08:12 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,7 @@
 import streamlit as st
+import pandas as pd
+import plotly.express as px
+import requests
 
 # Set page configuration
 st.set_page_config(
@@ -9,27 +12,25 @@ st.set_page_config(
 )
 
 # Title
-st.title("Sample Streamlit App with 3 Full-Width Tabs")
+st.title("Sample Streamlit App with Airdrop Data Visualization")
 
-# Tabs
-st.sidebar.title("Tabs")
+# Load data
+url = "https://api.flipsidecrypto.com/api/v2/queries/5b6a2816-65cb-47a1-b2b9-39318a79889f/data/latest"
+response = requests.get(url)
+data = response.json()
 
-with st.sidebar.container():
-    tab1 = st.button("Tab 1 dasd asd as", key="tab1")
-with st.sidebar.container():
-    tab2 = st.button("Tab 2", key="tab2")
-with st.sidebar.container():
-    tab3 = st.button("Tab 3", key="tab3")
+# Create a DataFrame
+df = pd.DataFrame(data)
 
-# Content
-if tab1:
-    st.header("Tab 1da ad d as das dads ")
-    # Add your content for Tab 1 here
-elif tab2:
-    st.header("Tab 2")
-    # Add your content for Tab 2 here
-elif tab3:
-    st.header("Tab 3")
-    # Add your content for Tab 3 here
-else:
-    st.header("Select a tab from the sidebar.")
+# Display data as a table
+st.header("Airdrop Data")
+st.write(df)
+
+# Visualize data
+st.header("Airdrop Amount Distribution")
+fig1 = px.pie(df, values="WALLETS", names="AIRDROP_AMOUNT", title="Number of Wallets per Airdrop Amount Range")
+st.plotly_chart(fig1)
+
+st.header("Airdrop Volume Distribution")
+fig2 = px.pie(df, values="AIRDROP_VOLUME", names="AIRDROP_AMOUNT", title="Airdrop Volume per Airdrop Amount Range")
+st.plotly_chart(fig2)
